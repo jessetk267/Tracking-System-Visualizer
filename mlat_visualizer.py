61d0b675716c7c16423c17695f2512b3affce82c
jessetk2-hash <jessetk267@gmail.com>
2026-01-06 18:33:07 -1000

    Added range_vectors for the visuals

--- mlat_visualizer.py
+++ mlat_visualizer.py
@@ -5,7 +5,6 @@ import colorcet as cc
 from matplotlib import colors
 
 # if target point is constrained to 0 <= x <= 1, etc.
-
 references = np.array([
     [0.0, 0.0, 0.0],
     [1.0, 0.0, 0.0],
@@ -15,9 +14,17 @@ references = np.array([
 
 true_point = np.array([0.5, 0.5, 0.5], dtype=np.float64)
 
+#Important variables
 ranges = cmake_mlat.find_ranges(true_point, references)
 
-print(ranges)
+range_vectors = cmake_mlat.find_range_vectors(true_point, references)
+
+estimated_point = cmake_mlat.find_point(references, ranges)
+
+scalar_error = cmake_mlat.find_scalar_error(true_point, estimated_point)
+
+vector_error = cmake_mlat.find_error_vector(true_point, estimated_point)
+
 
 pl = pv.Plotter()
 pl.show_grid()
@@ -30,6 +37,10 @@ def add_points(point, color, point_size, render_points_as_spheres):
         render_points_as_spheres = render_points_as_spheres
     )
 add_points(references, 'black', 20, True)
+add_points(true_point, 'blue', 20, True)
+
+for i in range(len(range_vectors)):
+    pl.add_arrows(references[i], range_vectors[i], mag=1.0)
 
 #Final Setups
 pl.camera_position = [
